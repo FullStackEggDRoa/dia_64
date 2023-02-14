4608ac65c984d8eaa7f6cfd8f6bf57d52a54708d
Daniel Roa <droa@apgslt.com>
2023-02-13 22:37:56 -0500

    Ejercicio 4 - Avance

---
+++ Ejercicio_4/src/ejercicio_4/Ejercicio_4.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_4;
+
+import Entidades.Pelicula;
+import Utilidades.utilidadesPelicula;
+import Servicios.serivciosPelicula;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author droa
+ */
+public class Ejercicio_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<Pelicula> peliculas = new ArrayList<>();
+        serivciosPelicula sP = new serivciosPelicula();
+        
+        sP.crearPelicula(peliculas);
+        
+        System.out.println("Películas Registradas:  ");
+        System.out.println(peliculas.toString().replace(",", ""));
+        
+        System.out.println("Filtro: Películas mayor a 1 hora");
+        sP.filtroDuracion(peliculas);
+        
+        System.out.println("Ordenado: De Mayor a Menor Duración:");
+        peliculas.sort(utilidadesPelicula.compararDuracionDESC);        
+        System.out.println(peliculas.toString().replace(",", ""));
+        
+        System.out.println("Ordenado: De Menor a Mayor Duración:");
+        peliculas.sort(utilidadesPelicula.compararDuracionASC);
+        System.out.println(peliculas.toString().replace(",", ""));
+        
+        System.out.println("Ordenado: Titulo (A-Z):");
+        peliculas.sort(utilidadesPelicula.compararTituloAZ);
+        System.out.println(peliculas.toString().replace(",", ""));
+        
+        System.out.println("Ordenado: Director (A-Z):");
+        peliculas.sort(utilidadesPelicula.comparardirectorAZ);
+        System.out.println(peliculas.toString().replace(",", ""));
+        
+    }
+    
+}
